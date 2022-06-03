64995f399ceff8d9a877c2318ad0b7ef7b59d284
moL3sz <molnarok2313@gmail.com>
2022-06-03 16:32:35 +0200

    pattern indexing done, correct padding fixed

--- js/main.js
+++ js/main.js
@@ -1,41 +1,77 @@
-const s = ( sketch ) => {
+const s = (sketch) => {
 
     let x = 100;
     let y = 100;
     let r = 40;
+    let d;
+
     sketch.setup = () => {
         sketch.createCanvas(window.innerWidth, window.innerHeight);
-        setTimeout(draw2, 1000)
+        d = setTimeout(draw2, 1000)
     };
+
+    let hW = window.innerWidth / 2;
+    let hH = window.innerHeight / 2;
     let pIndex = 0;
-    let t1,t2;
 
-    let draw2 = ()=>{
+    let draw2 = () => {
         sketch.background(255);
-        sketch.fill(255,0,0);
-        sketch.ellipse(2*r + 2, 2*r+2, 2*r);
 
-        setTimeout(draw2, 1000)
+        if (pIndex !== patterns.length) {
+
+            sketch.fill(255, 0, 0);
+            let [xx, yy] = pos2vec(patterns[pIndex])
+            console.log(xx, yy);
+
+            let [px, py] = paddings(r + 20, yy, xx);
+            sketch.ellipse(xx * hW + px, yy * hH + py, 2 * r);
+            pIndex++;
+        } else {
+            const end = document.createElement("div");
+            end.textContent = "Vége"
+            end.className = "end";
+            document.body.appendChild(end);
+            clearTimeout(d);
+            return
+        }
+        d = setTimeout(draw2, 1000)
     }
-    sketch.draw = async () => {
-
-
-    };
-};
-
-const pos2vec =(n) => {
-        //decomposite
-    return [Math.floor(n / 3), n % 3]
 }
 
-let patterns = [0, 2 ,6 ,8]
-/*
+const pos2vec = (n) => {
+    //decomposite
+    return [n % 3, Math.floor(n / 3)]
+}
 
-    O O O  |  0 1 2
-    O O O  |  3 4 5
-    O O O  |  6 7 8
+let paddings = (P, i, j) => {
+    const k = [
+        [
+            [P, P],
+            [0, P],
+            [-P, P]
+        ],
+        [
+            [P, 0],
+            [0, 0],
+            [-P, 0]
+        ],
+        [
+            [P, -P],
+            [0, -P],
+            [-P, -P]
+        ],
 
-    screen poses:
-    top left , top center , top right etc...
+    ]
+    return k[i][j]
+}
+let patterns = [0, 2, 4, 6, 8]
+    /*
 
- */
+        O O O  |  0 1 2
+        O O O  |  3 4 5
+        O O O  |  6 7 8
+
+        screen poses:
+        top left , top center , top right etc...
+
+     */
